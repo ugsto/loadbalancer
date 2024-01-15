91429ae1805200c826ae3ef1d78db1827bacb3b6
André Bortoli <andre@bortoli.phd>
2024-01-14 22:15:58 -0300

    refactor: improve way of write 2^16

--- src/main/java/com/bortoli/loadbalancer/utils/NetworkUtils.java
+++ src/main/java/com/bortoli/loadbalancer/utils/NetworkUtils.java
@@ -3,6 +3,6 @@ package com.bortoli.loadbalancer.utils;
 public class NetworkUtils {
 
   public static boolean validatePort(Integer port) {
-    return port.intValue() > 0 && port.intValue() < 65536;
+    return port.intValue() > 0 && port.intValue() < (1 << 16);
   }
 }
